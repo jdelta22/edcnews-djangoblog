f4f7a6922f4e838a3c1e57b375bb3ba1345eb868
jotao22 <jotao22gm@gmail.com>
2025-09-02 11:18:50 -0300

    logo e links de categoria adicionados

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jotao\Desktop\zipdpro\npdjango\venv
+set VIRTUAL_ENV=C:\Users\Fábrica\Desktop\projetosjoao\python\edcnews-djangoblog-main\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
